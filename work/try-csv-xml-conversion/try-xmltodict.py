aaa21c82f9afdfc41f6d4cc6539b3f9f0fc87cd9
jpnm-panda <jpnm0415shkm@gmail.com>
2022-02-05 12:26:16 +0900

    削除分のファイルをコミット

--- work/try-csv-xml-conversion/try-xmltodict.py
+++
@@ -1,9 +0,0 @@
-# 必要なモジュールをインポートする
-import xmltodict
-
-# XMLの値を辞書に変換する
-with open('conversion-test.xml', encoding='utf-8') as fd:
-
-    dict_data = xmltodict.parse(fd.read())
- 
-    print(dict_data)
